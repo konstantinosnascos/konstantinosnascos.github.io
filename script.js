6b82662e4b2cdd08a702c2cca4cfd9a84550ec1e
kostas900402 <nascoskostas@gmail.com>
2026-01-04 15:32:35 +0100

    ändrar layout så kort inte blir för stort med expand btn

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ images.forEach(img => {
         this.classList.toggle('enlarged');
     });
 });
+
+document.querySelectorAll(".project-toggle").forEach(button => {
+    button.addEventListener("click", () => {
+        const card = button.closest(".project-card");
+
+        card.classList.toggle("project-card--expanded");
+
+        button.textContent =
+            card.classList.contains("project-card--expanded")
+                ? "Visa mindre"
+                : "Visa mer";
+    });
+});
